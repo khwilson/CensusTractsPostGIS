bce51d40a82f854af08a0b29480240b840f4286d
Kevin Wilson <khwilson@gmail.com>
2016-12-09 14:47:06 +0000

    Script to grab tracts data from census website

---
+++ get_tracts.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import posixpath
+
+import lxml
+import requests
+
+BASE_URL = 'http://www2.census.gov/geo/tiger/TIGER2012/TRACT/'
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+    return local_filename
+
+
+def main():
+    r = requests.get(BASE_URL)
+
+    for filename in lxml.xpath('//a[starts-with(@href, "tl_2012_")]/@href'):
+        print("Downloading", filename)
+        download_file(posixpath.join(BASE_URL, filename))
+
+
+if __name__ == '__main__':
+    main()
